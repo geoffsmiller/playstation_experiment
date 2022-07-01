52d3b232753c86a00ecbeaa6db2e6fcbcb9aced8
Geoff Miller <geoffrey.s.miller@gmail.com>
2022-06-30 18:56:10 -0700

    good enough Redump scraping

--- the_psx_experiment/games/scrapers/redump/redump/spiders/game_disc_spider.py
+++ the_psx_experiment/games/scrapers/redump/redump/spiders/game_disc_spider.py
@@ -2,9 +2,10 @@ import scrapy
 
 class GameDiscSpider(scrapy.Spider):
     name = 'game_discs'
+    BASE_URL = "http://redump.org"
 
     def start_requests(self):
-        urls = ['http://redump.org/discs/system/psx/']
+        urls = [f'{self.BASE_URL}/discs/system/psx/']
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_list)
 
@@ -12,14 +13,23 @@ class GameDiscSpider(scrapy.Spider):
         game_links = response.xpath('//div[@class="gamesblock"]/table/tr/td/a/@href').getall()
 
         for game_link in game_links:
-            yield response.follow(game_link, self.parse_game)
+            yield response.follow(game_link, self.parse_game_disc)
 
-    def parse_game(self, response):
+        next_link = response.xpath('//div[@class="pages"]/ul/li/a[text()=">>"]/@href').get()
+        next_link = f'{self.BASE_URL}{next_link}'
+        if next_link:
+            yield scrapy.Request(url=next_link, callback=self.parse_list)
+
+    def parse_game_disc(self, response):
         titles = []
+        region = 'Unknown'
+        serials = ['Unknown']
+        editions = ['Unknown']
+        exe_date = '1900-01-01'
+
         game_div = response.xpath('//div[@id="main"]')
         titles.append(game_div.xpath('//h1/text()').get())
         titles.append(game_div.xpath('//h2/text()').get())
-        print(titles)
 
         gameinfo_table_rows = response.xpath('//table[@class="gameinfo"]/tr')
 
@@ -27,17 +37,23 @@ class GameDiscSpider(scrapy.Spider):
             header = row.xpath('.//th/text()').get()
             if header == 'Region':
                 region = row.xpath('.//td/a/img/@title').get()
-                print(region)
             if header == 'Serial':
                 serials = row.xpath('.//td/text()').get()
                 serials = serials.split(',')
                 serials = [serial.strip() for serial in serials]
-                print(serials)
             if header == 'EXE date':
                 exe_date = row.xpath('.//td/text()').get()
-                print(exe_date)
             if header == 'Edition':
                 editions = row.xpath('.//td/text()').get()
                 editions = editions.split(',')
                 editions = [edition.strip() for edition in editions]
-                print(editions)
+        discs = zip(serials, editions)
+        for disc in discs:
+            yield {
+                'title_1': titles[0],
+                'title_2': titles[1],
+                'region': region,
+                'exe_date': exe_date,
+                'serial': disc[0],
+                'edition': disc[1]
+            }
