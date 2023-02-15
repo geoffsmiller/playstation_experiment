ac3145ad446a20355ba404c3ae34885cf7cebabb
Geoff Miller <geoffrey.s.miller@gmail.com>
2023-02-14 20:16:30 -0800

    Saturn scrapers WIP

---
+++ the_psx_experiment/games/scrapers/redump_saturn/redump_saturn/spiders/game_disc_spider.py
@@ -0,0 +1,63 @@
+import scrapy
+
+class GameDiscSpider(scrapy.Spider):
+    name = "game_discs"
+    BASE_URL = "http://redump.org"
+
+    def start_requests(self):
+        urls = [f"{self.BASE_URL}/discs/system/ss/"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_list)
+
+    def parse_list(self, response):
+        game_links = response.xpath('//div[@class="gamesblock"]/table/tr/td/a/@href').getall()
+
+        for game_link in game_links:
+            yield response.follow(game_link, self.parse_game_disc)
+
+        next_link = response.xpath('//div[@class="pages"]/ul/li/a[text()=">>"]/@href').get()
+        next_link = f"{self.BASE_URL}{next_link}"
+        if next_link:
+            yield scrapy.Request(url=next_link, callback=self.parse_list)
+
+    def parse_game_disc(self, response):
+        titles = []
+        region = "Unknown"
+        serials = ["Unknown"]
+        editions = ["Unknown"]
+        build_date = "1900-01-01"
+        version = "Unknown"
+
+        game_div = response.xpath('//div[@id="main"]')
+        titles.append(game_div.xpath('//h1/text()').get())
+        titles.append(game_div.xpath('//h2/text()').get())
+
+        gameinfo_table_rows = response.xpath('//table[@class="gameinfo"]/tr')
+
+        for row in gameinfo_table_rows:
+            header = row.xpath(".//th/text()").get()
+            if header == "Region":
+                region = row.xpath(".//td/a/img/@title").get()
+            if header == "Serial":
+                serials = row.xpath(".//td/text()").get()
+                serials = serials.split(",")
+                serials = [serial.strip() for serial in serials]
+            if header == "Build date":
+                build_date = row.xpath(".//td/text()").get()
+            if header == "Version":
+                version = row.xpath(".//td/text()").get()
+            if header == "Edition":
+                editions = row.xpath(".//td/text()").get()
+                editions = editions.split(",")
+                editions = [edition.strip() for edition in editions]
+        discs = zip(serials, editions)
+        for disc in discs:
+            yield {
+                "title_1": titles[0],
+                "title_2": titles[1],
+                "region": region,
+                "build_date": build_date,
+                "version": version,
+                "serial": disc[0],
+                "edition": disc[1],
+            }
